7f7f2fcbeb689230ae26c108c9e5bbda0329996a
cayetopa <cayetopa960@gmail.com>
2021-09-08 20:43:47 -0500

    version 0.0

---
+++ Comercio_de_tecmnologia/Comercio_de_tecmnologia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Comercio_de_tecmnologia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Comercio_de_tecmnologia.settings')
+
+application = get_wsgi_application()
